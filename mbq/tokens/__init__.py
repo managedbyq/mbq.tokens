f95e6ee7307b30fcc26e015cb1da4976e0894b72
dursk <mmadurski@managedbyq.com>
2018-01-09 16:51:11 -0500

    Expose decode_with_unknown_header

--- mbq/tokens/__init__.py
+++ mbq/tokens/__init__.py
@@ -8,6 +8,7 @@ def init(certificate=None, allowed_audiences=None):
         allowed_audiences=allowed_audiences,
     )
 
-    global decode, decode_header
+    global decode, decode_header, decode_with_unknown_audience
     decode = decoder.decode
     decode_header = decoder.decode_header
+    decode_with_unknown_audience = decoder.decode_with_unknown_audience
--- tests/test_init.py
+++ tests/test_init.py
@@ -8,11 +8,14 @@ class InitTest(TestCase):
     def test_init(self):
         self.assertFalse(hasattr(tokens, 'decode'))
         self.assertFalse(hasattr(tokens, 'decode_header'))
+        self.assertFalse(hasattr(tokens, 'decode_with_unknown_audience'))
 
         tokens.init(certificate=keys.CERTIFICATE, allowed_audiences={'test'})
 
         self.assertTrue(hasattr(tokens, 'decode'))
         self.assertTrue(hasattr(tokens, 'decode_header'))
+        self.assertTrue(hasattr(tokens, 'decode_with_unknown_audience'))
 
         self.assertTrue(callable(tokens.decode))
         self.assertTrue(callable(tokens.decode_header))
+        self.assertTrue(callable(tokens.decode_with_unknown_audience))
